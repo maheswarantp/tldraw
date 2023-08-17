af5dfeb7e101bd3bb13c2782630c353fd486aca3
Maheswaran <maheswaran@pop-os.localdomain>
2023-08-17 18:05:35 +0530

    ColorPicker:V0.0.1

--- packages/tldraw/src/lib/tools/ColorPickerTool/ColorPickerTool.ts
+++ packages/tldraw/src/lib/tools/ColorPickerTool/ColorPickerTool.ts
@@ -1,19 +1,3 @@
-// import { StateNode } from '@tldraw/editor'
-// import { Erasing } from './children/Erasing'
-// import { Idle } from './children/Idle'
-// import { Pointing } from './children/Pointing'
-
-// /** @public */
-// export class ColorPickerTool extends StateNode {
-// 	static override id = 'colorpicker'
-// 	static override initial = 'idle'
-// 	static override children = () => [Idle, Pointing, Erasing]
-
-// 	override onEnter = () => {
-// 		this.editor.setCursor({ type: 'tool-colorpicker', rotation: 0 })
-// 	}
-// }
-
 import { DefaultColorStyle, HIT_TEST_MARGIN, StateNode, TLEventHandlers } from '@tldraw/editor'
 
 export class ColorPickerTool extends StateNode {
